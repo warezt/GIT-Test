d52cd823c6509c8a2f78a16c6a0ff3357b100f02
warezt <tanakorn.dhamalongkrot@gmail.com>
2017-02-25 15:05:25 +0700

    My First commit on this project

---
+++ path test git.R
@@ -0,0 +1,9 @@
+'http://r-bio.github.io/intro-git-rstudio/
+'Test2
+'test 1.58PM
+'test 2.02pm
+'Test 2.25pm
+'Test 2.26pm
+
+'git remote add origin https://github.com/warezt/GIT-Test
+'Another useful link https://www.git-tower.com/blog/workflow-of-version-control
