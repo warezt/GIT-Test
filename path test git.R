8c4cb6e4ac0359a04435f72e2c90bd46284057ec
warezt <tanakorn.dhamalongkrot@gmail.com>
2017-02-25 16:09:29 +0700

    Add link

--- path test git.R
+++ path test git.R
@@ -15,3 +15,4 @@ https://nicercode.github.io/git/branches.html
 ' Deleted test 3.09 and add this text'
 
 How to delete remote branch (Local or remote)'
+https://makandracards.com/makandra/621-git-delete-a-branch-local-or-remote
